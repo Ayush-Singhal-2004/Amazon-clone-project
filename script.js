4a4a4479360706ccda73d562606836a279c1d48c
Ayush-Singhal-2004 <ayushsinghal1904@gmail.com>
2024-01-12 21:53:36 +0530

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navSignIn = document.getElementById("nav-signin");
 navSignIn.addEventListener("mouseenter", () => {
     document.getElementById("signIn-popUp-id").style.opacity = 1;
 });
-const a = document.getElementById("signIn-popUp-id");
-a.addEventListener("mouseleave", () => {
-    a.style.opacity = 0;
+const signUpPopUpBox = document.getElementById("signIn-popUp-id");
+signUpPopUpBox.addEventListener("mouseleave", () => {
+    signUpPopUpBox.style.opacity = 0;
 });
